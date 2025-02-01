fbd8d20df70160894e20eb0c796a13be2481984e
vitali <vitali.krieger@stud.hn.de>
2025-02-01 20:30:28 +0100

    Debugging Test: Not Using full path but instead cmdName for SubprogramExecutor.execute()

--- src/ExternalCommand.cpp
+++ src/ExternalCommand.cpp
@@ -4,16 +4,8 @@
 #include "FileSearcher.h"
 
 void ExternalCommand::execute(std::vector<std::string> &args) const {
-    std::string pathToFile;
     try {
-        FileSearcher searcher;
-        pathToFile = searcher.getPathToFile(name);
-    } catch (const FileSearcher::FileNotFoundException&) {
-        throw CommandNotFoundException("Command not found: " + name);
-    }
-
-    try {
-        SubprogramExecutor executor(pathToFile, args);
+        SubprogramExecutor executor(name, args);
         executor.execute();
     } catch (const SubprogramExecutor::SubprogramExecutorException& e) {
         std::cerr << e.what() << std::endl;
