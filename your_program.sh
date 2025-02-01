1fe69497e7b24de666c1f281e4d4648cc200a20c
vitali <vitali.krieger@stud.hn.de>
2025-02-01 17:52:30 +0100

    Rücksetzung des echten terminals in den gebufferten zustand bei lokalem script.

--- your_program.sh
+++ your_program.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Use this script to run your program LOCALLY.
 #
@@ -12,14 +12,28 @@ set -e # Exit early if any commands fail
 #
 # - Edit this to change how your program compiles locally
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
-(
-  cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
-  cmake --build ./build
-)
+#(
+#  cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
+#  cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+#  cmake --build ./build
+#)
 
 # Copied from .codecrafters/run.sh
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
-exec ./build/shell "$@"
+
+stream_characters() {
+  while IFS= read -r -s -n1 character; do
+    if [[ "$character" = "" ]]; then
+      printf $'\n'
+    else
+      printf "$character"
+    fi
+  done
+}
+
+# Stellt sicher, dass stty sane nach Beenden der Shell ausgeführt wird
+trap 'stty sane' EXIT
+./cmake-build-debug/shell "$@" < <(stream_characters 2> /dev/null)
+
